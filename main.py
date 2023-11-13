0aac6dcfa361d9fad5b80d223ebb3a8824d48f97
ixiaolu2023 <148319690+ixiaolu2023@users.noreply.github.com>
2023-11-13 23:15:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import requests, json, re, os
 
 session = requests.session()
 # 机场的地址
-url = os.environ.get('URL')
+url = os.environ.get('https://cylink.club')
 # 配置用户名（一般是邮箱）
-email = os.environ.get('EMAIL')
+email = os.environ.get('buhu88@163.com')
 # 配置用户名对应的密码 和上面的email对应上
-passwd = os.environ.get('PASSWD')
+passwd = os.environ.get('dage1234')
 # server酱
 SCKEY = os.environ.get('SCKEY')
 
